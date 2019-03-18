75e185dfd2f13e4315f2a844fbc3f2d67e6c2f7d
Ignacio <ignaciosenese@gmail.com>
2019-03-19 04:53:10 +0900

    hovering grid

---
+++ app.js
@@ -0,0 +1,15 @@
+
+// var square1 = false;
+// var square1 = document.getElementsByClassName("square");
+// square1[0].addEventListener("click", function () {
+//   document.getElementsByClassName("square1").innerHTML = 'X'
+// });
+// document.getElementsByClassName("square").onclick = function(event) { document.getElementsByClassName("square1").innerHTML = 'X' }
+
+// for (var i = 0; i < square.length; i++) {
+//   square[i].addEventListener('click', function(event) {
+//     document.getElementsByClassName("square1").innerHTML = 'X'
+//   });
+// }
+
+function handleClick(event) { document.getElementsByClassName("square.square1").innerHTML = 'X' }
