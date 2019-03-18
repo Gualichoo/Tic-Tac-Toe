dea39e26da9ac3ab34b5004d540b6b732c9d19d2
Ignacio <ignaciosenese@gmail.com>
2019-03-19 06:04:48 +0900

    update

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
+//on Click do something
 
+// let button = document.querySelectorAll("button");
+// for (var i = 0; i < button.length; i ++)
+// button[i].addEventListener("click", () => {
+//   style.backgroundColor = "red";
+// });
+
+document.querySelectorAll('button').forEach(function(e) {
+  e.addEventListener('click', function() {
+    this.style.backgroundColor = "red";
+    this.innerHTML = 'X'
+    this.style.fontSize = '13px';
+    this.style.fontWeight = 'bold'
+    this.style.color = "black";
+  })
+});
 // var square1 = false;
 // var square1 = document.getElementsByClassName("square");
 // square1[0].addEventListener("click", function () {
 //   document.getElementsByClassName("square1").innerHTML = 'X'
 // });
-// document.getElementsByClassName("square").onclick = function(event) { document.getElementsByClassName("square1").innerHTML = 'X' }
+// document.getElementsByClassName("button").onclick = function(event) { document.getElementsByClassName("square").innerHTML = 'X' }
 
 // for (var i = 0; i < square.length; i++) {
 //   square[i].addEventListener('click', function(event) {
@@ -12,4 +28,10 @@
 //   });
 // }
 
-function handleClick(event) { document.getElementsByClassName("square.square1").innerHTML = 'X' }
+//  function handleClick(event) {
+//   if (event.type === 'click') {
+//   document.getElementsByClassName("square.square1").innerHTML = 'X' }
+//  }
+
+
+
